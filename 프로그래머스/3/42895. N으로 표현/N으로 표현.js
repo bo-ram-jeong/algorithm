af485f8b82e352057fd48ccc0186f6fbbc23392e
Boram Jeong <boram33377@gmail.com>
2024-08-23 16:22:00 +0900

    [level 3] Title: N으로 표현, Time: 17.58 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,29 +1,31 @@
 function solution(N, number) {
-    const dp = Array.from({ length: 9 }, () => new Set()); // dp[i]는 N을 i번 사용해서 만들 수 있는 숫자들의 집합
+  //dp: N을 i번 사용해서 만들 수 있는 숫자들의 집합
+  const dp = Array.from(new Array(9), () => new Set());
+  if (N == number) return 1;
+  else {
+    //N을 여러번 이어붙인 수 초기화
+    dp.forEach((element, index) => {
+      if (index !== 0) element.add(Number(String(N).repeat(index)));
+    });
 
-    // N을 여러 번 이어붙인 수를 초기화
+    // N을 1부터 8까지 사용하여 만들 수 있는 모든 숫자 계산
     for (let i = 1; i <= 8; i++) {
-        dp[i].add(Number(N.toString().repeat(i))); // 예: N=5라면 dp[3]에 555 추가
+      for (let j = 1; j < i; j++) {
+        for (const arg1 of dp[j]) {
+          for (const arg2 of dp[i - j]) {
+            dp[i].add(arg1 + arg2);
+            dp[i].add(arg1 - arg2);
+            dp[i].add(arg1 * arg2);
+            dp[i].add((arg1 / arg2) >> 0);
+          }
+        }
+      }
+
+      //최소값 리턴
+      if (dp[i].has(number)) return i;
     }
 
-    // 동적 계획법으로 해결
-    for (let i = 1; i <= 8; i++) { // N을 1번부터 8번까지 사용
-        for (let j = 1; j < i; j++) { // i를 두 그룹으로 나누어 처리
-            for (let op1 of dp[j]) {
-                for (let op2 of dp[i - j]) {
-                    dp[i].add(op1 + op2); // 덧셈
-                    dp[i].add(op1 - op2); // 뺄셈
-                    dp[i].add(op1 * op2); // 곱셈
-                    if (op2 !== 0) dp[i].add(Math.floor(op1 / op2)); // 나눗셈
-                }
-            }
-        }
-        // 만약 number가 dp[i]에 존재한다면 최소값은 i
-        if (dp[i].has(number)) {
-            return i;
-        }
-    }
-
-    // number를 만들 수 없으면 -1 반환
+    //8번만에 number 못 찾으면 -1 반환
     return -1;
+  }
 }
