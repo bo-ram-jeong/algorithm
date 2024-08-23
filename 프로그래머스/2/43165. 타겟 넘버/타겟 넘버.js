033f58a0e1178c963a9c005240a5ab2d241026df
Boram Jeong <boram33377@gmail.com>
2024-08-23 18:32:10 +0900

    [level 2] Title: 타겟 넘버, Time: 113.36 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,23 @@
+function solution(numbers, target) {
+  let count = 0;
+
+  function dfs(index, currentSum) {
+    if (index === numbers.length) {
+      if (currentSum === target) {
+        count++;
+      }
+      return;
+    }
+
+    // 더하기 연산
+    dfs(index + 1, currentSum + numbers[index]);
+
+    // 빼기 연산
+    dfs(index + 1, currentSum - numbers[index]);
+  }
+
+  // 초기 호출
+  dfs(0, 0);
+
+  return count;
+}
