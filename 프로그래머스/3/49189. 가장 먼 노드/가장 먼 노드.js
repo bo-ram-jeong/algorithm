f623743fd401d8ea6a2530a0cd39072d871bca5f
Boram Jeong <boram33377@gmail.com>
2024-08-26 21:00:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 2977.01 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,30 @@
+function solution(n, edge) {
+  const dist = [1, 1]; // 노드 번호를 인덱스로 쓰기 위해 1번 인덱스까지 1로 초기화
+ 
+  const graph = Array.from({ length: n + 1 }, () => []);
+
+  edge.forEach(([from, to]) => {
+    graph[from].push(to);
+    graph[to].push(from);
+  });
+
+  const queue = [1]; // 시작 노드 1을 넣어서 초기화
+
+  // 큐에 노드가 남아있을 동안
+  while (queue.length) {
+    // 현재 꺼낸 노드
+    const cur = queue.shift();
+    // 현재 꺼낸 노드와 연결된 노드를 돌면서
+    graph[cur].forEach((v) => {
+      // 확인 중인 노드가 방문하지 않은 노드라면
+      // 방문 처리해주면서 해당 노드까지의 거리를 현재 노드까지의 거리 + 1로 설정
+      if (!dist[v]) {
+        dist[v] = dist[cur] + 1;
+        queue.push(v); // 연결된 노드를 큐에 넣어줌
+      }
+    });
+  }
+
+  const result = dist.filter((v) => v === Math.max(...dist));
+  return result.length;
+}
