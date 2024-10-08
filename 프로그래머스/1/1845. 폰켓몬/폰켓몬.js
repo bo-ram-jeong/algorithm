a83eb67aee25823a9a4776395a090af33d99fd0f
Boram Jeong <boram33377@gmail.com>
2024-10-08 19:56:28 +0900

    [level 1] Title: 폰켓몬, Time: 17.41 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,12 @@
 function solution(numbers) {
-  const maxNum = Math.floor(numbers.length / 2);
-  const numbersMap = new Map();
+  const maxNum = Math.floor(numbers.length / 2); // 최대 고를 수 있는 수
+  const numbersMap = new Map(); // 숫자 등장 횟수를 저장할 Map 객체
 
-  numbers.forEach((num, index) => {
-    if (!numbersMap.has(num)) {
-      numbersMap.set(num, 0);
-    }
-    numbersMap.set(num, numbersMap.get(num) + 1);
+  // 각 숫자의 등장 횟수를 Map에 저장
+  numbers.forEach((num) => {
+    numbersMap.set(num, (numbersMap.get(num) || 0) + 1);
   });
 
-  return numbersMap.size > maxNum ? maxNum : numbersMap.size;
+  // Map의 사이즈와 maxNum 중 작은 값을 반환
+  return Math.min(numbersMap.size, maxNum);
 }
