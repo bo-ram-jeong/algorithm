735211fb339dec547bf471270ef68c95cb8bea38
Boram Jeong <boram33377@gmail.com>
2024-10-09 22:25:40 +0900

    [level 2] Title: 가장 큰 수, Time: 170.18 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,12 +1,7 @@
 function solution(numbers) {
-  // 숫자 배열을 문자열로 변환 후 정렬
-  const sorted = numbers
-    .map(num => num.toString()) // 숫자를 문자열로 변환
-    .sort((a, b) => (b + a) - (a + b)); // 이어붙였을 때 큰 순서대로 정렬
+  const sortedNumbers = numbers
+    .map((num) => num.toString())
+    .sort((a, b) => b + a - (a + b));
 
-  // 정렬된 배열을 하나의 문자열로 합침
-  const result = sorted.join('');
-
-  // 만약 결과가 '0'으로 시작하면, 이는 모든 값이 0이었음을 의미
-  return result[0] === '0' ? '0' : result;
+  return sortedNumbers[0] === '0' ? '0' : sortedNumbers.join('');
 }
