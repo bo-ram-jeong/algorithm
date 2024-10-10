2069424129d42bc025b0bdfd0f3ffd3565eb6800
Boram Jeong <boram33377@gmail.com>
2024-10-11 00:30:33 +0900

    [level 2] Title: 모음 사전, Time: 29.85 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,20 @@
 function solution(word) {
-    const vowels = ['A', 'E', 'I', 'O', 'U'];
-    const weight = [781, 156, 31, 6, 1]; // 각 자리에서의 가중치
+  const vowels = ['A', 'E', 'I', 'O', 'U'];
+  const words = [];
 
-    let result = 0;
+  // 재귀적으로 가능한 모든 단어 생성
+  function createWord(currentWord, length) {
+    if (length > 5) return; // 최대 길이가 5이므로, 5 이상이면 종료
+    words.push(currentWord); 
 
-    for (let i = 0; i < word.length; i++) {
-        const index = vowels.indexOf(word[i]); // 현재 문자의 인덱스
-        result += index * weight[i] + 1; // 가중치 계산
+    // 모음들로 새로운 단어를 생성
+    for (let vowel of vowels) {
+      createWord(currentWord + vowel, length + 1);
     }
+  }
 
-    return result;
+  // 빈 문자열부터 시작해서 가능한 모든 단어 생성
+  createWord('', 0);
+    
+  return words.indexOf(word);
 }
